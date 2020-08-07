6321617ff1e5a61fe95e60a11dd4ee1e471bb418
ASUS <Edwardo_tp@hotmail.com>
2020-08-06 20:13:16 -0600

    Se agrego el log out en el menu, tambien se agrego el cuadro de dialogo de AcercaDe la APP, y url para redireccionar a pagina de facebook

--- app/src/main/java/com/example/cursoavanzado/MainActivity.java
+++ app/src/main/java/com/example/cursoavanzado/MainActivity.java
@@ -1,18 +1,33 @@
 package com.example.cursoavanzado;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.util.Linkify;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Objects;
+
+import static com.example.cursoavanzado.metodosglobales.obtenerversionApp;
 
 
 public class MainActivity extends AppCompatActivity {
 //////////////////////////////////Vistas///////////////////////////
     Toolbar mainToolbar;
     Menu menuActivity;
+    Context context;
+    //Objetos
+    AlertDialog acercaDedialogo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +39,8 @@ public class MainActivity extends AppCompatActivity {
     private void configuracionesIniciales() {
         mainToolbar=findViewById(R.id.toolbarMain);
         setSupportActionBar(mainToolbar);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayShowTitleEnabled(false);
+        context= MainActivity.this;
     }
 /*
 Infla la vista del menu que dise;amos (main menu)
@@ -42,11 +59,30 @@ ademas se iguala el menuActivity al menu de la vista inflada
         int id=item.getItemId();// obtenemos el id del item presionado
         switch (id){
             case R.id.mainMenu_logout:
+                mydata mydata=new mydata(context);
+                mydata.setLogeo(false);
+                startActivity(new Intent(context,LoadActivity.class));
                 break;
             case R.id.AcercaDeAPP:
+                mostrarCuadroDialogoAcercaDe();
                 break;
 
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private void mostrarCuadroDialogoAcercaDe() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        ViewGroup parent = findViewById(R.id.parent);
+        View vista= getLayoutInflater().inflate(R.layout.acerca_de_dialogo,parent,false);
+        TextView txtversion= vista.findViewById(R.id.txtDialogoVersion);
+        TextView txtsigueme = vista.findViewById(R.id.txtsigueme);
+        Linkify.addLinks(txtsigueme,Linkify.WEB_URLS);//se agrega direccion url para redireccionar
+        Button btnllamar = vista.findViewById(R.id.btn_llamar);//TODO falta codigo pararealizar llamada
+        txtversion.setText(obtenerversionApp(context));
+        builder.setView(vista);//Se configura la vista en el builder
+        acercaDedialogo=builder.create();//se crea el cuadro de dialogo a partir del builder
+        acercaDedialogo.show();//muestra cuadro de dialogo
+
     }
 }
