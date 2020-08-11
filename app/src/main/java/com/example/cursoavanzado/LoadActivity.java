a6c61c2c0b74e9f3fac514319e73c8b78ee19e1a
ASUS <Edwardo_tp@hotmail.com>
2020-08-10 19:14:20 -0600

    Se agrega funcionalidad de los permisos

--- app/src/main/java/com/example/cursoavanzado/LoadActivity.java
+++ app/src/main/java/com/example/cursoavanzado/LoadActivity.java
@@ -1,26 +1,33 @@
 package com.example.cursoavanzado;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.Timer;
 import java.util.TimerTask;
 
+import static android.Manifest.permission.CAMERA;
 import static com.example.cursoavanzado.metodosglobales.obtenerversionApp;
 
 public class LoadActivity extends AppCompatActivity {
 ////////////////////////////////////VISTAS///////////////
     TextView txtversion,txtinfo;
+    Button btnPermisos;
 /////////////////////////////////////////////////////////
 
 ////////////////////////////////////UTILIDADES///////////
 Context context;
 Timer primerTimer,segundoTimer;
+boolean permisoCamara= false;
 /////////////////////////////////////////////////////////
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,12 +35,50 @@ Timer primerTimer,segundoTimer;
         setContentView(R.layout.activity_load);
         txtinfo=findViewById(R.id.txt_load_info);
         txtversion=findViewById(R.id.txt_version);
+        btnPermisos=findViewById(R.id.btn_permisos);
         context= LoadActivity.this;
         String version= obtenerversionApp(context);
         txtversion.setText(version);
-        setprimerTimer(3000);
+        revisarPermisos();
+
 
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        btnPermisos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                revisarPermisos();
+                btnPermisos.setVisibility(view.INVISIBLE);
+            }
+        });
+    }
+
+    private void revisarPermisos(){
+    permisoCamara= checkSelfPermission(CAMERA)== PackageManager.PERMISSION_GRANTED;
+    if (permisoCamara){
+        setprimerTimer(3000);
+        }else {
+        requestPermissions(new String[]{CAMERA},25);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode==25){
+            permisoCamara= checkSelfPermission(CAMERA)== PackageManager.PERMISSION_GRANTED;
+            if(permisoCamara){
+                setprimerTimer(3000);
+            }else{
+                txtinfo.setText(getString(R.string.alertapermisos));
+                btnPermisos.setVisibility(View.VISIBLE);
+            }
+        }
+    }
+
     private void  setprimerTimer (int milis){
         primerTimer= new Timer(); //Declarando el timer como uno nuevo
         primerTimer.schedule(new TimerTask() {
