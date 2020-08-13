3cd367cb059e3659cd676547e04dc351b3e354c0
ASUS <Edwardo_tp@hotmail.com>
2020-08-12 20:09:23 -0600

    Se agrega activity de objeto para pedir usuario,pass,correo,genero,puesto

---
+++ app/src/main/java/com/example/cursoavanzado/Usuario.java
@@ -0,0 +1,68 @@
+package com.example.cursoavanzado;
+
+public class Usuario {
+    private int id;
+    private String nombre;
+    private String password;
+    private String genero;
+    private String rol;
+    private String correo;
+
+
+    public Usuario(int id, String nombre, String password, String genero, String rol, String correo) {
+        this.id = id;
+        this.nombre = nombre;
+        this.password = password;
+        this.genero = genero;
+        this.rol = rol;
+        this.correo = correo;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+}
